967db7aef4606548242145f552951529e85af4aa
bondarji05 <150642890+bondarji05@users.noreply.github.com>
2024-02-05 10:40:21 +0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,27 @@
+
+const anchors = document.querySelectorAll('a[href^="#foo7ter"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+$(function() {
+ let header = $('.header');
+
+ $(window).scroll(function() {
+   if($(this).scrollTop() > 1) {
+    header.addClass('header_fixed');
+   } else {
+    header.removeClass('header_fixed');
+   }
+ });
+});
